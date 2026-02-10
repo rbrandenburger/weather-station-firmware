049a5a6871d080c8aae575b5407e42b8a959011f
rbrandenburger@huskers.unl.edu <rbrandenburger@huskers.unl.edu>
2026-02-10 12:24:09 -0600

    update api route

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     recorded_on = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
     r = requests.post(
-        f"{os.environ['WEATHER_APP_URL']}/readings/create",
+        f"{os.environ['WEATHER_APP_URL']}/api/readings",
         json={
             "temp_c": temp_c,
             "relative_humidity": humidity,
