67ebb6ab0b600c3490371b9eb9936c52de4703ee
rbrandenburger@huskers.unl.edu <rbrandenburger@huskers.unl.edu>
2026-02-10 19:52:40 -0600

    update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-import Adafruit_DHT
+import adafruit_sht31d
+import board
 import os
 import datetime
 import requests
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
     print("taking a reading")
-    humidity, temp_c = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 14)
+
+    load_dotenv()
+
+    i2c = board.I2C()
+    sensor = adafruit_sht31d.SHT31D(i2c)
+
+    humidity = sensor.relative_humidity
+    temp_c = sensor.temperature
+
     recorded_on = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
     r = requests.post(
--- sht_sensor.py
+++
@@ -1,8 +0,0 @@
-import adafruit_sht31d
-import board
-
-i2c = board.I2C()
-sensor = adafruit_sht31d.SHT31D(i2c)
-
-print(f"Celsius: {sensor.temperature}")
-print(f"Humidity: {sensor.relative_humidity}")
